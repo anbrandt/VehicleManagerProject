a61f79fd260c7110636e5b8791286ca6a9baaa48
anbrandt <andrzej.brandt@gmail.com>
2017-05-08 18:36:55 +0200

    another commit

--- src/com/andrzej/VehicleManager.java
+++ src/com/andrzej/VehicleManager.java
@@ -7,12 +7,12 @@ public class VehicleManager {
 
 	//methods that manages Vehicles
 
-
+//coś tam
 	// create a list of vehicles that are in garage and list of vehicles that are on road
 	//add a vehicle
 	//send a vehicle to somewhere
 	//search vehicle by regnumber, driver, brand, typeofcar
 
 	//use extends
-	
+
 }
