8790246b02d961b1f03c12583adf337131a495c3
anbrandt <andrzej.brandt@gmail.com>
2017-05-09 17:40:35 +0200

    car and vehicle classes

--- src/com/andrzej/Car.java
+++ src/com/andrzej/Car.java
@@ -3,5 +3,9 @@ package com.andrzej;
 /**
  * Created by andrzej on 08.05.17.
  */
-public class Car {
+public class Car extends Vehicle {
+	public Car(Integer weight) {
+		super();
+	}
+
 }
--- src/com/andrzej/Vehicle.java
+++ src/com/andrzej/Vehicle.java
@@ -9,23 +9,23 @@ public class Vehicle {
 
 	//use setter for mileage and driver in order not to extend the size of constructor in the Vehicle class
 	private Integer weightOfTheVehicle;
-	private String typeOfTheVehicle;
 	private Integer mileageOfTheVehicle;
+	private String typeOfTheVehicle; 
 	private String brandOfTheVehicle;
 	private String registrationNumber;
 	private GpsTracker gpsPosition; //call the GpsTracker class to create field gpsposition
 	private Driver driver; //call the Driver class to create field driver
 
-
+	//don't know if the access modifier should be public ????
 
 	//constructor for the Vehicle class
 	public Vehicle(Integer weightOfTheVehicle, String typeOfTheVehicle, Integer mileageOfTheVehicle,
 				   String brandOfTheVehicle, String registrationNumber) {
 		this.weightOfTheVehicle = weightOfTheVehicle;
-		this.typeOfTheVehicle = typeOfTheVehicle;
 		this.mileageOfTheVehicle = mileageOfTheVehicle;
 		this.brandOfTheVehicle = brandOfTheVehicle;
 		this.registrationNumber = registrationNumber;
+
 	}
 
 	public Integer getWeightOfTheVehicle() {
